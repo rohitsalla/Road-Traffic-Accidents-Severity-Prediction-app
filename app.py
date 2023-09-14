81c1b144873373e0804700ffcbc88997a9781293
rohitsalla <sallarohit1011@gmail.com>
2023-09-14 20:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,102 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import joblib
-from sklearn.ensemble import RandomForestClassifier
-from prediction import get_prediction, ordinal_encoder
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+import os
+from flask_cors import CORS, cross_origin
+from prediction_Validation_Insertion import pred_validation
+from trainingModel import trainModel
+from training_Validation_Insertion import train_validation
+import flask_monitoringdashboard as dashboard
+from predictFromModel import prediction
+import json
 
-model = joblib.load(r'Model/Baseline_RandomForest_final.joblib')
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
-st.set_page_config(page_title="Accident Severity Prediction App", page_icon="🚧", layout="wide")
-
-# creating an option list for the drop down menus
-
-options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-options_age = ['18-30', '31-50', 'Over 51', 'Unknown', 'Under 18']
-
-options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas',
-       ' Industrial areas', 'School areas', '  Recreational areas',
-       ' Outside rural areas', ' Hospital areas', '  Market areas',
-       'Rural village areas', 'Unknown', 'Rural village areasOffice areas',
-       'Recreational areas']
-       
-options_cause = ['No distancing', 'Changing lane to the right',
-       'Changing lane to the left', 'Driving carelessly',
-       'No priority to vehicle', 'Moving Backward',
-       'No priority to pedestrian', 'Other', 'Overtaking',
-       'Driving under the influence of drugs', 'Driving to the left',
-       'Getting off the vehicle improperly', 'Driving at high speed',
-       'Overturning', 'Turnover', 'Overspeed', 'Overloading', 'Drunk driving',
-       'Unknown', 'Improper parking']
-options_vehicle_type = ['Automobile', 'Lorry (41-100Q)', 'Other', 'Pick up upto 10Q',
-       'Public (12 seats)', 'Stationwagen', 'Lorry (11-40Q)',
-       'Public (13-45 seats)', 'Public (> 45 seats)', 'Long lorry', 'Taxi',
-       'Motorcycle', 'Special vehicle', 'Ridden horse', 'Turbo', 'Bajaj', 'Bicycle']
-options_driver_exp = ['5-10yr', '2-5yr', 'Above 10yr', '1-2yr', 'Below 1yr', 'No Licence', 'unknown']
-options_lanes = ['Two-way (divided with broken lines road marking)', 'Undivided Two way',
-       'other', 'Double carriageway (median)', 'One way',
-       'Two-way (divided with solid lines road marking)', 'Unknown']
-
-features = ['hour','day_of_week','casualties','accident_cause','vehicles_involved','vehicle_type','driver_age','accident_area','driving_experience','lanes']
-
-st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
-
-def main():
-    with st.form('prediction_form'):
-        st.subheader("Enter the input for following features:")
-
-        hour = st.slider("Hour of Accident: ", 0, 23, value=0, format="%d")
-        day_of_week = st.selectbox("Select Day of the Week: ", options=options_day)
-        casualties = st.slider("No of Casualties: ", 1,8, value=0, format="%d")
-        accident_cause = st.selectbox("Cause of Accident: ", options=options_cause)
-        vehicles_involved = st.slider("Vehicles Involved: ", 1, 7, value=0, format="%d")
-        vehicle_type = st.selectbox("Vehicle Type: ", options=options_vehicle_type)
-        driver_age = st.selectbox("Driver Age: ", options=options_age)
-        accident_area = st.selectbox("Accident Area", options=options_acc_area)
-        driving_experience = st.selectbox("Driving Experience: ", options=options_driver_exp)
-        lanes = st.selectbox("Lane of Accident: ", options=options_lanes)
-        
-        submit = st.form_submit_button("Predict")
+app = Flask(__name__)
+dashboard.bind(app)
+CORS(app)
 
 
-    if submit:
-        day_of_week = ordinal_encoder(day_of_week, options_day)
-        accident_cause = ordinal_encoder(accident_cause, options_cause)
-        vehicle_type = ordinal_encoder(vehicle_type, options_vehicle_type)
-        driver_age =  ordinal_encoder(driver_age, options_age)
-        accident_area =  ordinal_encoder(accident_area, options_acc_area)
-        driving_experience = ordinal_encoder(driving_experience, options_driver_exp) 
-        lanes = ordinal_encoder(lanes, options_lanes)
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRouteClient():
+    try:
+        if request.json is not None:
+            path = request.json['filepath']
+
+            pred_val = pred_validation(path) #object initialization
+
+            pred_val.prediction_validation() #calling the prediction_validation function
+
+            pred = prediction(path) #object initialization
+
+            # predicting for dataset present in database
+            path,json_predictions = pred.predictionFromModel()
+            return Response("Prediction File created at !!!"  +str(path) +'and few of the predictions are '+str(json.loads(json_predictions) ))
+        elif request.form is not None:
+            path = request.form['filepath']
+
+            pred_val = pred_validation(path) #object initialization
+
+            pred_val.prediction_validation() #calling the prediction_validation function
+
+            pred = prediction(path) #object initialization
+
+            # predicting for dataset present in database
+            path,json_predictions = pred.predictionFromModel()
+            return Response("Prediction File created at !!!"  +str(path) +'and few of the predictions are '+str(json.loads(json_predictions) ))
+        else:
+            print('Nothing Matched')
+    except ValueError:
+        return Response("Error Occurred! %s" %ValueError)
+    except KeyError:
+        return Response("Error Occurred! %s" %KeyError)
+    except Exception as e:
+        return Response("Error Occurred! %s" %e)
 
 
-        data = np.array([hour,day_of_week,casualties,accident_cause,vehicles_involved, vehicle_type,driver_age,accident_area,driving_experience,lanes]).reshape(1,-1)
 
-        pred = get_prediction(data=data, model=model)
+@app.route("/train", methods=['POST'])
+@cross_origin()
+def trainRouteClient():
 
-        st.write(f"The predicted severity is:  {pred[0]}")
+    try:
+        if request.json['folderPath'] is not None:
+            path = request.json['folderPath']
 
-if __name__ == '__main__':
-    main()
+            train_valObj = train_validation(path) # object initialization
+
+            train_valObj.train_validation() # calling the training_validation function
+
+
+            trainModelObj = trainModel() # object initialization
+            trainModelObj.trainingModel() # training the model for the files in the table
+
+
+    except ValueError:
+
+        return Response("Error Occurred! %s" % ValueError)
+
+    except KeyError:
+
+        return Response("Error Occurred! %s" % KeyError)
+
+    except Exception as e:
+
+        return Response("Error Occurred! %s" % e)
+    return Response("Training successfull!!")
+
+#port = int(os.getenv("PORT",5000))
+if __name__ == "__main__":
+ #   host = '0.0.0.0'
+    #port = 5000
+  #  httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+   # httpd.serve_forever()
+   main()
